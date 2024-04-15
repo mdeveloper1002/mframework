7031dc8e8b026acd5de3cda55989f0f55d6f8602
mdeveloper1002 <cabbar09@proton.me>
2024-04-15 07:59:26 +0300

    hex added enc

--- src/Helpers/Encryption.php
+++ src/Helpers/Encryption.php
@@ -17,4 +17,18 @@ class Encryption
         $iv = $_ENV['IV'];
         return openssl_decrypt($data, 'aes-256-cbc', $key, 0, $iv);
     }
+
+    public static function hexEncrypt(string $data): string
+    {
+        $key = $_ENV['SECRET_KEY'];
+        $iv = $_ENV['IV'];
+        return bin2hex(openssl_encrypt($data, 'aes-256-cbc', $key, 0, $iv));
+    }
+
+    public static function hexDecrypt(string $data): string
+    {
+        $key = $_ENV['SECRET_KEY'];
+        $iv = $_ENV['IV'];
+        return openssl_decrypt(hex2bin($data), 'aes-256-cbc', $key, 0, $iv);
+    }
 }
